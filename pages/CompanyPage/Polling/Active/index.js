2fb6d7217cb2a3ec70b904c4498158b6c83ff4b5
ramdhaniakbar <ramdhaniakbarmuhamad@gmail.com>
2022-10-11 10:42:04 +0700

    mengubah poling active & nonactive

--- pages/CompanyPage/Polling/Active/index.js
+++ pages/CompanyPage/Polling/Active/index.js
@@ -1,68 +1,122 @@
 import React, { Component, useState } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import styles from "/styles/Polling/Active.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck, faCircleXmark } from "@fortawesome/free-regular-svg-icons";
+import { faEnvelope, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+	faCircleCheck,
+	faCircleXmark,
+} from "@fortawesome/free-regular-svg-icons";
 import CreateNewPoll from "./createNewPoll";
-import Navbar from "../../../Component/navbar";
+import { useRouter } from "next/router";
 
 const Active = () => {
-  return (
-    <div className={styles.body}>
-      <main className={styles.main} id="main">
-        <Navbar />
+	const router = useRouter();
+	const currentRoute = router.pathname;
 
-        <img src="/polling.png" className={styles.image}></img>
+	return (
+		<div className={styles.body}>
+			<main className={styles.main} id="main">
+				<nav className={styles.navbar}>
+					<ul className={styles.navList}>
+						<li className={styles.navItem}>
+							<Image
+								className={styles.imageLogo}
+								src="/EasyHandler_Hitam.png"
+								alt="logo"
+								width={42}
+								height={42}
+							/>
+						</li>
+						<li className={styles.navItem}>
+							<FontAwesomeIcon className={`${styles.navIcon}`} icon={faEnvelope} />
+						</li>
+						<li className={styles.navItem}>
+							<FontAwesomeIcon className={`${styles.navIcon}`} icon={faUser} />
+						</li>
+					</ul>
+				</nav>
 
-        <div className={`d-flex justify-content-end mt-3 me-5`}>
-          <a className={`${styles["btn-polls"]} btn btn-sm btn-outline-secondary`} type="button" data-bs-toggle="modal" data-bs-target="#modalCreatePoll">
-            CREATE NEW POLLS
-          </a>
-        </div>
+				<img src="/polling.png" className={styles.image}></img>
 
-        <main className={styles.mainContent}>
-          <div className={`${styles["bg-content"]} mt-5`}>
-            <div className={`${styles["content"]}`}>
-              <div className="container-fluid">
-                <div className="row">
-                  <div className={`${styles["sidebar"]} col-3`}>
-                    <div className="d-flex flex-column text-center">
-                      <Link href="./Active">
-                      <a className={`${styles["btn-sidebar"]} px-5 py-5`}>
-                        <div className={`${styles["icon-poll"]}`}>
-                          <FontAwesomeIcon icon={faCircleCheck} />
-                        </div>
-                        ACTIVE
-                      </a>
-                    </Link>
-                    <Link href="./NonActive">
-                      <a className={`${styles["btn-sidebar"]} px-5 py-5`}>
-                        <div className={`${styles["icon-poll"]}`}>
-                          <FontAwesomeIcon icon={faCircleXmark} />
-                        </div>
-                        NON-ACTIVE
-                      </a>
-                    </Link>
-                    </div>
-                  </div>
-                  <div className={`${styles["polls"]} col-9`}>
-                    <h3 className={`${styles["text-head"]} border-bottom`}>Active Poll</h3>
-                    <h2>No Polls</h2>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
+				<div className={`d-flex justify-content-end mt-3 me-5`}>
+					<a
+						className={`${styles["btn-polls"]} btn btn-sm btn-outline-secondary`}
+						type="button"
+						data-bs-toggle="modal"
+						data-bs-target="#modalCreatePoll"
+					>
+						CREATE NEW POLLS
+					</a>
+				</div>
 
-        <div class="modal fade" id="modalCreatePoll" tabindex="-1" aria-labelledby="modalCreateNewPoll" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered">
-            <CreateNewPoll />
-          </div>
-        </div>
-      </main>
-    </div>
-  );
-}
+				<main className={`${styles["main-content"]}`}>
+					<div className={`${styles["bg-content"]} mt-5`}>
+						<div className={`${styles["content"]}`}>
+							<div className="container-fluid">
+								<div className={`${styles["row-sidebar"]} row`}>
+									<div className={`${styles["sidebar"]} col-2`}>
+										<div className="d-flex flex-column text-center">
+											<Link href="./Active">
+												<div
+													className={
+														currentRoute === "/CompanyPage/Polling/Active"
+															? `${styles["btn-tasks"]} ${styles["active"]}`
+															: `${styles["btn-tasks"]}`
+													}
+												>
+													<div className={`${styles["icon-poll"]}`}>
+														<FontAwesomeIcon icon={faCircleCheck} />
+													</div>
+													<span>ACTIVE</span>
+												</div>
+											</Link>
+											<Link href="./NonActive">
+												<div
+													className={
+														currentRoute === "/CompanyPage/Polling/NonActive"
+															? `${styles["btn-tasks"]} ${styles["active"]}`
+															: `${styles["btn-tasks"]}`
+													}
+												>
+													<div className={`${styles["icon-poll"]}`}>
+														<FontAwesomeIcon icon={faCircleXmark} />
+													</div>
+													<span>NON-ACTIVE</span>
+												</div>
+											</Link>
+										</div>
+									</div>
+									<div className="col-10">
+										<div className={`${styles["title-poll"]}`}>
+											<span className={`${styles["title"]}`}>Active Poll</span>
+											<hr />
+										</div>
+										<div className={`${styles["data-poll"]} text-center`}>
+											<span>No Polls</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</main>
+
+				<div
+					class="modal fade"
+					id="modalCreatePoll"
+					tabindex="-1"
+					aria-labelledby="modalCreateNewPoll"
+					aria-hidden="true"
+				>
+					<div class="modal-dialog modal-dialog-centered">
+						<CreateNewPoll />
+					</div>
+				</div>
+			</main>
+		</div>
+	);
+};
 
 export default Active;
